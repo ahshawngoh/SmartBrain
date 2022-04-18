ded5eaa7fb55e12a0faaab7edc4546cdba97064e
unknown <ahshawngoh@gmail.com>
2022-04-18 20:52:16 +0800

    change host url, add npm serve

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://serene-cove-41855.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
